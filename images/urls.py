335985bc17239feb3568b6df6e698140d6c7c8a4
JKMamusements <jkmamusements@gmail.com>
2024-06-04 22:15:41 +0530

    update url

--- images/urls.py
+++ images/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.home, name='home'),
     path('index/', views.index_view, name='images-index'),
     path('upload/', views.upload_view, name='images-upload'),
     path('test/', views.testing, name='test'),
-    path('', views.home, name='home'),
     
 ]
